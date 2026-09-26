62a2417c475345ca7360fd6ff39b3da047b946fa
Yomna Mostafa <yomnamostafaesmail@gmail.com>
2026-09-26 17:22:28 +0200

    Add submit solution page with modals and leaderboard

---
+++ src/data/leaderboardData.js
@@ -0,0 +1,12 @@
+export const leaderboard = [
+  { rank: 1, name: "rahulsenna", isMe: false },
+  { rank: 2, name: "alok-2007", isMe: false },
+  { rank: 3, name: "feliposz", isMe: false },
+  { rank: 4, name: "yanruijie902136", isMe: false },
+  { rank: 5, name: "cornishon", isMe: false },
+  { rank: 4439, name: "JuanMartinCoder", isMe: false },
+  { rank: 4440, name: "skn054", isMe: false },
+  { rank: 4441, name: "yahia997", isMe: true },
+  { rank: 4442, name: "x595036279", isMe: false },
+  { rank: 4443, name: "erikcik", isMe: false },
+];
